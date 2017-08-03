a78df1debc09aa5eea09aaa66e1f3f4407ce3f62
Gagan Gulati <gagangulati333@gmail.com>
2017-08-03 12:32:06 +0530

    conditional statement introduced

--- SpyRecord.py
+++ SpyRecord.py
@@ -1,6 +1,9 @@
 print "Let's get started!"
 spy_name = raw_input("Enter your name :")
-spy_salutation = raw_input("enter your salutation")
-#spy identity created
+spy_salutation = raw_input("enter your salutation :")
+#check wether spy input somthing or not
+if len(spy_name) > 0:
+    #code blocks if condition is true
+#spy name and salutation concatinated
 spy_name = spy_name + " " + spy_salutation
 print "WELCOME " + spy_name + " glad to have you back with us"
