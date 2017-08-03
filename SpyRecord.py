04689600df7150f69e5fb12950f8c2c4683e1a93
Gagan Gulati <gagangulati333@gmail.com>
2017-08-03 12:12:50 +0530

    spy record is created

--- SpyRecord.py
+++ SpyRecord.py
@@ -1,2 +1,6 @@
 print "Let's get started!"
-spy_name = raw_input("Enter spy name")
+spy_name = raw_input("Enter your name :")
+spy_salutation = raw_input("enter your salutation")
+#spy identity created
+spy_name = spy_name + " " + spy_salutation
+print "WELCOME " + spy_name + " glad to have you back with us"
