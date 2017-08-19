3a33f92a4db5bf9307ad25969feeb8984c9d055f
Gagan Gulati <gagangulati333@gmail.com>
2017-08-19 16:09:48 +0530

    whole code converted in dictionaries

--- spy_details.py
+++ spy_details.py
@@ -1,6 +1,8 @@
 # details of default user.
-spy_name = 'bond'
-spy_salutation = 'Mr.'
-spy_age = 24
-spy_rating = 4.7
-spy_is_online = True
+spy = {
+    'name' : 'Bond',
+    'salutation' : 'Mr.',
+    'age' : 24,
+    'rating' : 4.7,
+    'is_online' : True
+}
